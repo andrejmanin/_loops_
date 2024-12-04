3b5b0ec2ae749876763dd45c5e101ed0a41d39d5
andrejmanin <andrejmanin44@gmail.com>
2024-12-04 17:51:35 +0200

    Innit commit

--- loops/main.cpp
+++ loops/main.cpp
@@ -1,25 +1,71 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "ukr");
+void drawFigure(char type) {
+    int size = 5;
 
-    int lower, upper, number;
-
-    cout << "¬вед≥ть нижню межу: ";
-    cin >> lower;
-    cout << "¬вед≥ть верхню межу: ";
-    cin >> upper;
-
-    do {
-        cout << "¬вед≥ть число в д≥апазон≥ [" << lower << ", " << upper << "]: ";
-        cin >> number;
-        if (number < lower || number > upper) {
-            cout << "„исло не входить у д≥апазон. —пробуйте ще раз.\n";
+    switch (type) {
+    case 'a':
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) cout << "*";
+            cout << "\n";
         }
-    } while (number < lower || number > upper);
+        break;
 
-    cout << "¬и ввели число: " << number << ", €ке знаходитьс€ в д≥апазон≥!\n";
+    case 'b':
+        for (int i = size; i > 0; i--) {
+            for (int j = 0; j < i; j++) cout << "*";
+            cout << "\n";
+        }
+        break;
+
+    case 'v':
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j >= i) cout << "*";
+                else cout << " ";
+            }
+            cout << "\n";
+        }
+        break;
+
+    case 'g':
+        for (int i = size - 1; i >= 0; i--) {
+            for (int j = 0; j < size; j++) {
+                if (j >= i) cout << "*";
+                else cout << " ";
+            }
+            cout << "\n";
+        }
+        break;
+
+    case 'd':
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j || i + j == size - 1) cout << "*";
+                else cout << " ";
+            }
+            cout << "\n";
+        }
+        break;
+
+    default:
+        cout << "Invalid choice!\n";
+    }
+}
+
+int main() {
+    char choice;
+    do {
+        cout << "Select a figure to display (a, b, v, g, d): ";
+        char figureType;
+        cin >> figureType;
+
+        drawFigure(figureType);
+
+        cout << "Do you want to select another figure? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
